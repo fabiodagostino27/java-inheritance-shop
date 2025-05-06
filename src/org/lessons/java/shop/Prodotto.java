720966c61302347b7bcca4a1f992a360bf813c90
fabiodagostino27 <fabiodago27@gmail.com>
2025-05-06 19:07:49 +0200

    ADD: Prodotto class with attributes and constructor

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,21 @@
 package org.lessons.java.shop;
 
-public class Prodotto {
+import java.math.BigDecimal;
+import java.util.Random;
 
+public class Prodotto {
+    private int codice;
+    private String nome;
+    private String marca;
+    private BigDecimal prezzo;
+    private BigDecimal iva;
+
+    public Prodotto(String nome, String marca, BigDecimal prezzo, BigDecimal iva) {
+        Random rand = new Random();
+        this.codice = rand.nextInt(999);
+        this.nome = nome;
+        this.marca = marca;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
 }
