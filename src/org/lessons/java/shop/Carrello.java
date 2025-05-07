f0bf4718819f775984275b8eeceda7208a99ac85
fabiodagostino27 <fabiodago27@gmail.com>
2025-05-07 16:19:26 +0200

    ADD: Implement initial product input functionality in Carrello class

--- src/org/lessons/java/shop/Carrello.java
+++ src/org/lessons/java/shop/Carrello.java
@@ -1,7 +1,13 @@
 package org.lessons.java.shop;
 
+import java.util.Scanner;
+
 public class Carrello {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quanti prodotti vuoi mettere nel carrello? ");
+        int numeroProdotti = sc.nextInt();
+        int[] lunghezzaArrayProdotti = new int[numeroProdotti];
     }
 }
